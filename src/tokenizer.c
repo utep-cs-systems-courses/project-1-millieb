9d77931ba4e0dd6c0423b7ec746152c1846b0048
millieb <mildred.brito@outlook.com>
2020-06-25 17:55:25 -0600

    space_char function ready

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
+#include "tokenizer.h"
 
-int main () {
-
-  char input[100];
-
-  printf(">");
-
-  /*Used fgets instead of scanf in order to be able to print input string with spaces*/
-  fgets(input,sizeof(input), stdin); 
-  printf("%s",input);
-
-
+int space_char(char c)
+{
+  if (c == ' ' || c == '\t')
+    return 1;
 
   return 0;
 }
