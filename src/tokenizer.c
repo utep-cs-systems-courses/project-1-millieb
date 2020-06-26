74e8d34aa208c2479a69fb01aa9ed7d7f84669b2
millieb <mildred.brito@outlook.com>
2020-06-25 18:14:55 -0600

    Improved space_char()

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -5,6 +5,18 @@ int space_char(char c)
 {
   if (c == ' ' || c == '\t')
     return 1;
-
+  
+  else if (c == '\n')
+    return 0;
+  
+  else
   return 0;
+}
+
+int main()
+{
+  int result = space_char('m');
+  printf("%i\n", result);
+  int result2 = space_char(' ');
+  printf("%i\n", result2);
 }
