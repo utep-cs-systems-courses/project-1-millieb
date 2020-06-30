b48511e7fdd4ec3ab76a1dc12f13a6ac90be7b52
millieb <mildred.brito@outlook.com>
2020-06-30 04:06:50 -0600

    created history.c and added init_history()

---
+++ src/history.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+#include "history.h"
+
+typedef struct s_Item {
+  int id;
+  char *str;
+  struct s_Item *next; //helps unite two structures of the same type
+
+} Item;
+
+typedef struct s_List {
+  struct s_Item *root;
+
+} List;
+
+
+
+/* Initialize the linked list to keep the history. */
+List* init_history() 
+{
+  List *head = (List*)malloc(sizeof(List)); //allocating memory
+  head -> root = NULL; 
+  return head;
+}
+
+/* Add a history item to the end of the list.
+   List* list - the linked list
+   char* str - the string to store
+*/
+void add_history(List *list, char *str)
+{
+  
+}
+
+
+
+/* Retrieve the string stored in the node where Item->id == id.
+   List* list - the linked list
+   int id - the id of the Item to find */
+char *get_history(List *list, int id);
+
+
+
+/* Print the entire contents of the list. */
+void print_history(List *list);
+
+/*Free the history list and the strings it references. */
+void free_history(List *list);
