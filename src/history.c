dd0a94bbdde17f98844c930476a6b6db50532fad
millieb <mildred.brito@outlook.com>
2020-06-30 04:48:36 -0600

    added print_history()

--- src/history.c
+++ src/history.c
@@ -7,15 +7,13 @@ typedef struct s_Item {
   int id;
   char *str;
   struct s_Item *next; 
-
 } Item;
 
 typedef struct s_List {
   struct s_Item *root;
-
 } List;
 
-/* Initialize the linked list to keep the history. */
+/*Initialize the linked list to keep the history*/
 List* init_history() 
 {
   List *head;
@@ -52,16 +50,14 @@ void add_history(List *list, char *str)
 }
 
 
+void print_history(List *list)
+{
+  
+  while(list != NULL)
+    {
+      printf("%s\n", list->str);
+      list = list->next; //to iterate
+    }
+}
 
-/* Retrieve the string stored in the node where Item->id == id.
-   List* list - the linked list
-   int id - the id of the Item to find */
-char *get_history(List *list, int id);
 
-
-
-/* Print the entire contents of the list. */
-void print_history(List *list);
-
-/*Free the history list and the strings it references. */
-void free_history(List *list);
