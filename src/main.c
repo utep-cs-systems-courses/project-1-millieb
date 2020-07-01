21738b89a9342cc0a2b462457c49958edc0435b3
millieb <mildred.brito@outlook.com>
2020-07-01 15:13:03 -0600

    added UI to main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 #include "history.h"
 
-
-int main () {
-
+void main()
+{
   char input[100];
-  printf(">");
 
-  /*Used fgets instead of scanf in order to be able to print input string with spaces*/
-  fgets(input,sizeof(input), stdin);
-  printf("%s",input);
-  
-  return 0;
+  while(1)
+    {
+      printf("<h> to see history\n<c> to clear history\nEnter <e> to close program\nStart writing to enter test\n>");
+      fgets(input, sizeof(input), stdin);
+
+      if(input[0] == 'c')
+	{
+	  printf("Entered clear history\n\n");
+	}
+      else if(input[0] == 'h')
+	{
+	  printf("Entered view history\n\n");
+	}
+      else if(input[0] == 'e')
+	{
+	  exit(0);
+	}
+      else
+	{
+	  printf("Text entered is: %s\n\n", input);
+	}
+    }
 }
